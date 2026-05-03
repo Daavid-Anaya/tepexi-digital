1aedaf421c346122060837018fe4df050bfb5912
David Anaya <daavidanaya@gmail.com>
2026-05-03 11:08:14 -0600

    feat(analytics): add GoogleAnalytics component and update SpeedInsights import

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,7 +1,8 @@
 import { Navbar } from '@/components/layout/Navbar'
 import { Footer } from '@/components/layout/Footer'
 import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function SiteLayout({
   children,
@@ -13,6 +14,7 @@ export default function SiteLayout({
       <Navbar />
       <main className="min-h-screen">{children}</main>
       <Footer />
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ''} />
       <Analytics />
       <SpeedInsights />
     </>
